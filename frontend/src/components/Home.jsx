d0d79f2cd86b5d4d2c9e970a7180db76226b9506
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2024-07-27 23:15:33 +0530

    Updated Home page styles

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -14,21 +14,33 @@ function Home() {
 
   return (
     <div>
-      <div className="search-bar-container">
-        <img src={Logo} alt="Logo" className="logo" /><p className='logo-title'>Compas</p>
-        <input type="text" className="search-input" placeholder="Search..." />
-        <TbMapPinSearch className="search-icon" />
-        <FaBars className="hamburger-menu" onClick={toggleSidebar} />
+      <div className="search-bar-container !px-5">
+        <div className='flex justify-center items-center'>
+          <img src={Logo} alt="Logo" className="logo" /><p className='logo-title'>Compus</p>
+        </div>
         
+        <div className='search-bar flex items-center justify-center flex-1'>
+          <input type="text" className="search-input" placeholder="Search..." />
+          <span>
+            <TbMapPinSearch className="search-icon pe-1" />
+          </span>
+        </div>
+        
+        {
+          sidebarOpen 
+          ? <FaTimes className="close-icon" onClick={toggleSidebar} />
+          : <FaBars className="hamburger-menu" onClick={toggleSidebar} />
+        }
         <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
-          <FaTimes className="close-icon" onClick={toggleSidebar} />
           <ul>
             <li><a href="/signup">Signup</a></li>
             <li><a href="/login">Login</a></li>
           </ul>
         </div>
       </div>
+
       <MapComponent />
+
     </div>
   )
 }
--- frontend/src/components/MapComponent.jsx
+++ frontend/src/components/MapComponent.jsx
@@ -105,7 +105,11 @@ const FitBounds = () => {
 const MapComponent = () => {
   return (
     <div id="map">
-        <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
+        <MapContainer 
+            center={position} 
+            zoom={13} 
+            zoomControl = {false}
+            style={{ height: "100vh", width: "100%" }}>
             <TileLayer 
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
