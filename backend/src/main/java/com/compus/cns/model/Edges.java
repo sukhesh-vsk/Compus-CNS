9b3eb2a7f95fd28d7b91bae9b94894083d4be952
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2024-08-11 15:52:20 +0530

    feat(api): create controller, service for edges

--- backend/src/main/java/com/compus/cns/model/Edges.java
+++ backend/src/main/java/com/compus/cns/model/Edges.java
@@ -1,8 +1,5 @@
 package com.compus.cns.model;
 
-import org.hibernate.annotations.OnDeleteAction;
-import org.locationtech.jts.geom.GeometryFactory;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,9 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-
-import com.compus.cns.model.Nodes;
 
 @Entity
 public class Edges {
@@ -28,33 +22,37 @@ public class Edges {
     private Nodes node2;
     private double distance;
 
+    // Getters and Setters
     public double getDistance() {
         return distance;
     }
     public void setDistance(double distance) {
-        GeometryFactory geom = new GeometryFactory();
         this.distance = distance;
     }
-    // Getters and Setters
+
     public Long getEdgeID() {
         return edgeID;
     }
     public void setEdgeID(Long edgeID) {
         this.edgeID = edgeID;
     }
+    
     public Nodes getNode1() {
         return node1;
     }
     public void setNode1(Nodes node1) {
         this.node1 = node1;
     }
+    
     public Nodes getNode2() {
         return node2;
     }
     public void setNode2(Nodes node2) {
         this.node2 = node2;
     }
-
     
+    public String getEdgeDesc() {
+    	return node1.getDescription() + " to " + node2.getDescription();
+    }
 }
 
--- backend/src/main/java/com/compus/cns/service/EdgeService.java
+++ backend/src/main/java/com/compus/cns/service/EdgeService.java
@@ -1,8 +1,52 @@
 package com.compus.cns.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.compus.cns.model.Edges;
+import com.compus.cns.repository.EdgesRepo;
+import com.compus.cns.repository.NodeRepo;
 
 @Service
 public class EdgeService {
+    @Autowired
+    private EdgesRepo edgeRepo;
+    @Autowired
+    private NodeRepo nodeRepo;
     
+    public List<Edges> getAllEdges() {
+    	return edgeRepo.findAll();
+    }
+    
+    public Boolean createEdge(Long node1, Long node2) {
+    	Edges edge = new Edges();
+    	edge.setNode1(nodeRepo
+    					.findById(node1)
+    					.orElseThrow(() -> new RuntimeException("Node1 not found"))
+    					);
+    	edge.setNode2(nodeRepo
+    					.findById(node2)
+    					.orElseThrow(() -> new RuntimeException("Node2 not found"))
+    					);
+    	edge.setDistance(nodeRepo.getDistance(node1, node2));
+    	
+    	try {
+    		edgeRepo.save(edge);	
+    	} catch(Exception e) {
+    		return false;
+    	}
+    	
+    	return true;
+    }
+    
+    public Boolean createEdges(List<Edges> data) {
+    	try {
+    		edgeRepo.saveAllAndFlush(data);	
+    	} catch(Exception e) {
+    		return false;
+    	}
+    	return true;
+    }
 }
